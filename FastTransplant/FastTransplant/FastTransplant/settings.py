7e457fb8fbb2a95066bb34c064adf7428571f2d7
AnaBeatrizMassuh <aaamassuh@gmail.com>
2023-06-13 15:49:38 -0300

    Frontend

--- FastTransplant/FastTransplant/FastTransplant/settings.py
+++ FastTransplant/FastTransplant/FastTransplant/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'frontend.apps.FrontendConfig'
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
