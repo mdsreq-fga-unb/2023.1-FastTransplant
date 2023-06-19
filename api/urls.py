d02468509a22a6618ea53eca223b7e115067d99d
gitbmvb <bruno.martval@gmail.com>
2023-06-18 21:33:19 -0300

    Adding index and patients page

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_report, name='index'),
+    path('', views.index, name='index'),
     path('data', views.data, name='data'),
     path('panel', views.panel, name='panel'),
     path('donators', views.donator_list, name='donators'),
