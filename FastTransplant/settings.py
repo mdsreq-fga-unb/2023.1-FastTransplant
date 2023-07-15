68a15a7af26ca7bfd0f540f4c69823dbe5a6b718
gitbmvb <bruno.martval@gmail.com>
2023-07-15 00:33:50 -0300

    Addind static files - heroku spike

--- FastTransplant/settings.py
+++ FastTransplant/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
